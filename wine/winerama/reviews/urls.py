81a8d45d2d3e13c65afa4ca3bbf231b77e40e0be
Tikam Singh Alma <timonalma81@gmail.com>
2018-03-10 20:30:48 +0530

    urls.py

--- wine/winerama/reviews/urls.py
+++ wine/winerama/reviews/urls.py
@@ -7,10 +7,7 @@ urlpatterns = [
     url(r'^wine$', views.wine_list, name='wine_list'),
     url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
     url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
-    # ex: /review/user - get reviews for the logged user
     url(r'^review/user/(?P<username>\w+)/$',views.user_review_list,name='user_review_list'),
-    # ex: /review/user - get reviews for the user passed in the url
     url(r'^review/user/$',views.user_review_list,name='user_review_list'),
-    # ex: /recommendation - get wine recommendations for the logged user
     url(r'^recommendation/$', views.user_recommendation_list, name='user_recommendation_list'),
 ]
